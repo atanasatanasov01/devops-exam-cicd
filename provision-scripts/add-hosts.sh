8707d9183fa2bc72027a44e4356ea09b4dd365e2
Atanas Atanasov <atanasovvv01@gmail.com>
2022-11-25 22:32:55 +0200

    Init repo

--- provision-scripts/add-hosts.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "192.168.150.201 pipelines.do1.exam pipelines" >> /etc/hosts
-echo "192.168.150.202 containers.do1.exam containers" >> /etc/hosts
-echo "192.168.150.203 monitoring.do1.exam monitoring" >> /etc/hosts
